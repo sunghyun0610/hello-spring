358e3d6270468a828a0511c8180a5ca453082fb1
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2023-12-03 20:43:00 +0900

    model개념

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
     @GetMapping("hello")
-    public String hello(Model model){
+    public String hello(Model model){ //Model은 controller->view 데이터를 전달할 수 있는 데이터 컨테이너입니다
         model.addAttribute("data","hello!!");
+        //Model이라는 객체에 data라는 속성을 추가하였고 속성 값은 hello!!이다.
         return "hello";
 
     }
