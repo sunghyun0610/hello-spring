14210e7012b9e3a4fb60fa15d2ed32dd93f6de8e
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2023-12-04 18:45:33 +0900

    Getmapping

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("hello")///hello url을 입력하면 Get방식으로 url에 매칭됨 그리고 밑에 메서드 실행
     public String hello(Model model){ //Model은 controller->view 데이터를 전달할 수 있는 데이터 컨테이너입니다
         model.addAttribute("data","hello!!");
         //Model이라는 객체에 data라는 속성을 추가하였고 속성 값은 hello!!이다.
