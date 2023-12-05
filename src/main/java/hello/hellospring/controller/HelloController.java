1c8366bb5c31106979615059f9e056157a4e69d2
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2023-12-05 13:32:00 +0900

    Update HelloController.java

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -14,12 +14,8 @@ public class HelloController {
     }
 }
 /*
-<<<<<<< HEAD
- * controller에서 return값으로 문자르 반환하면 viewResolver가 화면을 찾아서 처리한다
- * viewName 매핑 : [resources:templates/] + {ViewName} + [.html]
- * */
-=======
+
+/*=======
 * controller에서 return값으로 문자르 반환하면 viewResolver가 화면을 찾아서 처리한다
 * viewName 매핑 : [resources:templates/] + {ViewName} + [.html]
 * */
->>>>>>> main
